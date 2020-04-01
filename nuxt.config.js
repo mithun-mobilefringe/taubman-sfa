06ebb2e3c70064f8e277ec32f46c58bda72be2eb
Mithun <mithun@mobilefringe.com>
2020-04-01 17:21:32 -0400

    added stores

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,52 +1,57 @@
+const pkg = require('./package')
 
+const webpack = require('webpack')
+const axios = require('axios')
+const API_URL = "https://mallmaverick.com/api/v4/taubman/"
+const MM_HOST = "https://halifaxcentre.mallmaverick.com"
 export default {
-  mode: 'spa',
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
-  /*
-  ** Global CSS
-  */
-  css: [
-  ],
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
-  /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-  ],
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-  ],
-  /*
-  ** Build configuration
-  */
-  build: {
+    mode: 'spa',
     /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
+     ** Headers of the page
+     */
+    head: {
+        title: process.env.npm_package_name || '',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
+    },
+    /* Set your environment variables here */
+    env: {
+        API_URL: API_URL,
+        MM_HOST: MM_HOST
+    },
+    /*
+     ** Customize the progress-bar color
+     */
+    loading: { color: '#fff' },
+    /*
+     ** Global CSS
+     */
+    css: [],
+    /*
+     ** Plugins to load before mounting the App
+     */
+    plugins: [],
+    /*
+     ** Nuxt.js dev-modules
+     */
+    buildModules: [],
+    /*
+     ** Nuxt.js modules
+     */
+    modules: [],
+    /*
+     ** Build configuration
+     */
+    build: {
+        /*
+         ** You can extend webpack config here
+         */
+        extend(config, ctx) {}
     }
-  }
 }
---
+++ store/state.js
@@ -0,0 +1,10 @@
+const state = {
+    api: {
+        site: null,
+        version: null
+    },
+    banners: null,
+    blogs: []
+}
+
+export default state
